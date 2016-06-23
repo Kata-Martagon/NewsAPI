76af17fec6ac6224da57e9cc17570cafad856a23
nogainbar <nogaenbar@MBPro-2.local>
2016-06-23 14:44:46 +0100

    commiting but stashin

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
 function urlBuilder(param1, param2, key) {
   const query = {
-    q : param1, //Search terms
+    q: param1, // Search terms
     fq: param2,
-    'api-key' : key
-  }
-  const baseUrlAndPath = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
-  var queryString = Object.keys(query).map(function (key) {
+    'api-key': key,
+  };
+  const baseUrlAndPath = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+  const queryString = Object.keys(query).map(function (key) {
     return key + '=' + query[key]
-  }).join('&')
+  }).join('&');
 
-  const url = baseUrlAndPath + '?' + queryString
-  return url
+  const url = baseUrlAndPath + '?' + queryString;
+  return url;
 }
 
-var url = urlBuilder('referendum', 'europe and britain', '8310a722a1af4fe39644eee195781143')
+const url = urlBuilder('referendum', 'europe and britain', '8310a722a1af4fe39644eee195781143');
 
-function parseData (obj) {
-  return obj.response.docs.map(function (article) {
-    return {
+function parseData(obj) {
+  return obj.response.docs.map(function(article) {
+    const a = {
       title: article.headline.main,
       link: article.web_url,
       snippet: article.snippet
     }
+    console.log(a);
   })
+
 }
 
 fetchAPI(url, 'GET', parseData)
