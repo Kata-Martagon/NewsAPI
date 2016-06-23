cfafdf317aba9ecce2d92e2ce1a8c1ba8f027d28
Matt <matthewsharp123@gmail.com>
2016-06-23 16:16:17 +0100

    publish api call on site

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 
 const buildArticleNode = template => article => {
   template.querySelector('h2').textContent = article.title;
-  template.querySelector('p').textContent = article.tagline;
+  template.querySelector('p').innerHTML = article.tagline;
 
   return document.importNode(template, true);
 };
@@ -20,12 +20,16 @@ function displayArticleNodes(articleNodes) {
 function main() {
   const query = {
     q: 'brexit',
+    'show-fields': 'standfirst',
     'api-key': guardianAPIKey,
   };
 
   const guardianUrl = createUrl('http://content.guardianapis.com/', ['search'], query);
 
+  const trace = data => { console.log(data); return data; };
+
   fetchAPI(guardianUrl)
+    .then(trace)
     .then(fromGuardianToStandardArticleFormat)
     .then(buildArticleNodes)
     .then(displayArticleNodes);
