fe60f62b635b6957c03b8ff0110f8f1d3c2ca889
Kara de la Marck <karadelamarck@gmail.com>
2016-06-23 14:06:04 +0100

    Style: Make app.js fit new style guide

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
-function urlBuilder(param1, param2, key) {
-  const query = {
-    q : param1, //Search terms
-    fq: param2,
-    'api-key' : key
-  }
-  const baseUrlAndPath = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
-  var queryString = Object.keys(query).map(function (key) {
-    return key + '=' + query[key]
-  }).join('&')
 
-  const url = baseUrlAndPath + '?' + queryString
-  return url
+function urlBuilder(param1, param2, apikey) {
+  const query = {
+    q: param1, // Search terms
+    fq: param2,
+    'api-key': apikey,
+  };
+  const baseUrlPath = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+  const queryString = Object.keys(query).map(key => `${key}=${query[key]}`
+  .join('&')
+
+  const url = `${baseUrlPath}?${queryString}`
+  return url;
 }
 
-var url = urlBuilder('referendum', 'europe and britain', '8310a722a1af4fe39644eee195781143')
+const url = urlBuilder('referendum', 'europe and britain', '8310a722a1af4fe39644eee195781143');
 
-function parseData (obj) {
-  return obj.response.docs.map(function (article) {
+function parseData(obj) {
+  return obj.response.docs.map(function(article) {
     return {
       title: article.headline.main,
       link: article.web_url,
-      snippet: article.snippet
-    }
-  })
+      snippet: article.snippet,
+    };
+  });
 }
 
-fetchAPI(url, 'GET', parseData)
+fetchAPI(url, 'GET', parseData);
