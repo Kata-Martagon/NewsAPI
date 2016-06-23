28a4297483a6b4daba09739dc91a74667057e955
Kara de la Marck <karadelamarck@gmail.com>
2016-06-23 14:33:33 +0100

    Fix: Made app.js to completely follow style guide

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* global fetchAPI */
 
 function urlBuilder(param1, param2, apikey) {
   const query = {
@@ -6,23 +7,21 @@ function urlBuilder(param1, param2, apikey) {
     'api-key': apikey,
   };
   const baseUrlPath = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-  const queryString = Object.keys(query).map(key => `${key}=${query[key]}`
-  .join('&')
+  const queryString = Object.keys(query).map(key => `${key}=${query[key]}`)
+  .join('&');
 
-  const url = `${baseUrlPath}?${queryString}`
+  const url = `${baseUrlPath}?${queryString}`;
   return url;
 }
 
 const url = urlBuilder('referendum', 'europe and britain', '8310a722a1af4fe39644eee195781143');
 
 function parseData(obj) {
-  return obj.response.docs.map(function(article) {
-    return {
-      title: article.headline.main,
-      link: article.web_url,
-      snippet: article.snippet,
-    };
-  });
+  return obj.response.docs.map(article => ({
+    title: article.headline.main,
+    link: article.web_url,
+    snippet: article.snippet,
+  }));
 }
 
 fetchAPI(url, 'GET', parseData);
