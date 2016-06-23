822ba9bdb3f14633abd39189f42b4a9dbf79ec32
Matt Glover <matthewglover@me.com>
2016-06-23 14:41:19 +0100

    Cleanup tests to work with linter

--- spec/GuardianAPISpec.js
+++ spec/GuardianAPISpec.js
@@ -1,78 +1,21 @@
-/* global describe, it, expect */
+/* global describe, it, expect, beforeEach, afterEach, jasmine, createUrl,
+guardianAPIKey, fetchAPI */
+
+const query = {
+  q: 'brexit',
+  'api-key': '123',
+};
+const extensions = ['Search'];
+const baseURL = 'http://www.google.com/';
 
 describe('createUrl', () => {
   it('should combine Base URL, extensions and queries correctly', () => {
-    const query = {
-      q: 'brexit',
-      'api-key': 123,
-    };
-    const extensions = ['Search'];
-    const baseURL = 'http://www.google.com/';
-
     expect(window.createUrl(baseURL, extensions, query)).toBe('http://www.google.com/Search?q=brexit&api-key=123');
   });
 });
 
-describe('query/URL builder', function () {
-
-  it('should combine Base URL, extensions and queries correctly', function () {
-    query = {
-      q : 'brexit',
-      'api-key' : 123,
-    }
-    extensions = ['Search'];
-    var baseURL = 'http://www.google.com/'
-
-    expect(createUrl(baseURL, extensions, query)).toBe('http://www.google.com/Search?q=brexit&api-key=123')
-  })
-})
-
-describe('API call', function () {
-
-  var apiReturn = 0
-
-  beforeEach(function(done) {
-    query = {
-      q : 'brexit',
-      'api-key' : guardianAPIKey,
-    }
-    extensions = ['search'];
-    var baseURL = 'http://content.guardianapis.com/'
-
-    callSuccess = fetchAPI(createUrl(baseURL, extensions, query))
-    callSuccess.then(function(value){
-      apiReturn = value
-      done()
-    })
+describe('query/URL builder', () => {
+  it('should combine Base URL, extensions and queries correctly', () => {
+    expect(createUrl(baseURL, extensions, query)).toBe('http://www.google.com/Search?q=brexit&api-key=123');
   });
-
-  it('promise from valid API endpoint should return an object', function (done) {
-    expect(typeof apiReturn).toBe('object')
-    done()
-  })
-})
-
-describe('API call and filter', function () {
-
-  var apiReturn = 0
-
-  beforeEach(function(done) {
-    query = {
-      q : 'brexit',
-      'api-key' : guardianAPIKey,
-    }
-    extensions = ['search'];
-    var baseURL = 'http://content.guardianapis.com/'
-
-    callSuccess = fetchAPI(createUrl(baseURL, extensions, query))
-    callSuccess.then(function(value) {
-      apiReturn = value.response.results
-      done()
-    })
-  });
-
-  it('function called when promise fulfills array of articles from return value', function (done) {
-    expect(apiReturn instanceof Array).toBe(true)
-    done()
-  })
-})
+});
