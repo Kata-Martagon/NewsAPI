3beb38c0d435810f7560b278461ccfcec3f90139
Kara de la Marck <karadelamarck@gmail.com>
2016-06-23 12:33:31 +0100

    Fix: remove commented out domManipulator func

--- js/app.js
+++ js/app.js
@@ -13,19 +13,6 @@ function urlBuilder(param1, param2, key) {
   return url
 }
 
-// var domManipulator = function(obj) {
-//   var articleInfo = obj.response.docs.map(function(article) {
-//
-//    return (
-//     '<div>Question Title: ' + article.headline.main + '</div>' +
-//     '<div>Link: <a href="' + article.web_url + '">Click here</a></div>' +
-//     '<div>Summary: ' + article.snippet + '</div>' +
-//     '</br>'
-//   )
-//   })
-//     document.getElementById('results-body').innerHTML = articleInfo.join('')
-// }
-
 var url = urlBuilder('referendum', 'europe and britain', '8310a722a1af4fe39644eee195781143')
 
 function parseData (obj) {
