3c303a3ffdbd6c98c8a93fb94db23dcfbd7ae36b
Matt <matthewsharp123@gmail.com>
2016-06-23 12:30:26 +0100

    added basic API call tests

--- spec/GuardianAPISpec.js
+++ spec/GuardianAPISpec.js
@@ -18,14 +18,54 @@ describe('query/URL builder', function () {
 
     expect(createUrl(baseURL, extensions, query)).toBe('http://www.google.com/Search?q=brexit&api-key=123')
   })
+})
 
+describe('API call', function () {
 
+  var apiReturn = 0
 
+  beforeEach(function(done) {
+    query = {
+      q : 'brexit',
+      'api-key' : guardianAPIKey,
+    }
+    extensions = ['search'];
+    var baseURL = 'http://content.guardianapis.com/'
 
+    callSuccess = fetchAPI(createUrl(baseURL, extensions, query))
+    callSuccess.then(function(value){
+      apiReturn = value
+      done()
+    })
+  });
 
+  it('promise from valid API endpoint should return an object', function (done) {
+    expect(typeof apiReturn).toBe('object')
+    done()
+  })
+})
 
+describe('API call and filter', function () {
 
+  var apiReturn = 0
 
+  beforeEach(function(done) {
+    query = {
+      q : 'brexit',
+      'api-key' : guardianAPIKey,
+    }
+    extensions = ['search'];
+    var baseURL = 'http://content.guardianapis.com/'
 
+    callSuccess = fetchAPI(createUrl(baseURL, extensions, query))
+    callSuccess.then(function(value) {
+      apiReturn = value.response.results
+      done()
+    })
+  });
 
+  it('function called when promise fulfills array of articles from return value', function (done) {
+    expect(apiReturn instanceof Array).toBe(true)
+    done()
+  })
 })
