420bb9bcf135190223506693a6e3abef3ffa0c9b
Kara de la Marck <karadelamarck@gmail.com>
2016-06-26 20:05:42 +0100

    Add Array of responsive dates for navDates display

--- src/main.js
+++ src/main.js
@@ -1,32 +1,18 @@
 /* global GuardianAPI, NYTAPI, DomUpdater */
 
-function fillZero(num) {
-  return num < 10 ? `0${num}` : num;
-}
-
-
-function formatGuardianDate(date) {
-  return `${date.getFullYear()}-${fillZero(date.getMonth() + 1)}-${fillZero(date.getDate())}`;
-}
-
-function formatNYTDate(date) {
-  return `${date.getFullYear()}${fillZero(date.getMonth() + 1)}${fillZero(date.getDate())}`;
-}
-
 function main() {
-  const guardianContentNode = document.getElementById('GuardianContent');
-  const NYTContentNode = document.getElementById('NYTimesContent');
-
   const today = new Date(Date.now());
   console.log(today);
-  // const yesterday = new Date(Date.now() - (8.64 * Math.pow(10, 7)));
-  // console.log(yesterday);
-  //
-  const datesOfLastWeek = Array.from({ length: 6 }, (_, idx) => new Date(Date.now() - (idx +1) * (8.64 * Math.pow(10, 7))))
-  //
-  // const vs = Array.from({length: 6}, (_, idx) => idx + 1);
-  // console.log(vs);
-  console.log(datesOfLastWeek);
+
+  const datesLastWeek = Array.from({ length: 7 }, (_, idx) => new Date(today - (idx + 1) * (8.64 * Math.pow(10, 7))));
+  console.log(datesLastWeek);
+
+  const navDates = datesLastWeek.map(el => el.toDateString().slice(4, 10));
+  console.log(navDates);
+
+
+  const guardianContentNode = document.getElementById('GuardianContent');
+  const NYTContentNode = document.getElementById('NYTimesContent');
 
   GuardianAPI.getArticles(formatGuardianDate(today))
     .then(DomUpdater.buildArticleNodes)
@@ -37,5 +23,17 @@ function main() {
     .then(DomUpdater.displayArticleNodes(NYTContentNode));
 }
 
-
 main();
+
+
+function fillZero(num) {
+  return num < 10 ? `0${num}` : num;
+}
+
+function formatGuardianDate(date) {
+  return `${date.getFullYear()}-${fillZero(date.getMonth() + 1)}-${fillZero(date.getDate())}`;
+}
+
+function formatNYTDate(date) {
+  return `${date.getFullYear()}${fillZero(date.getMonth() + 1)}${fillZero(date.getDate())}`;
+}
